6f490001746f83ac05ca118f7773ca15bb4225c9
SakshiTibrewal <tibrewalsakshi1309@gmail.com>
2021-07-19 12:00:06 +0530

    First commit

---
+++ config/chatrooms.js
@@ -0,0 +1,23 @@
+module.exports = [
+  {
+    name: 'Denmark',
+    image: 'chatrooms/Denmark.jpg'
+  },
+  {
+    name: 'Mumbai',
+    image: 'chatrooms/Mumbai.jpg'
+  },
+  {
+    name: 'New York',
+    image: 'chatrooms/New York.jpg'
+  },
+  {
+    name: 'Himachal Pradesh',
+    image: 'chatrooms/Himachal Pradesh.jpg'
+  },
+  {
+    name: 'Bangalore',
+    image: 'chatrooms/Bangalore.jpg'
+  },
+]
+
---
+++ config/users.js
@@ -0,0 +1,33 @@
+module.exports = [
+  {
+    name: 'Kangana',
+    lastName: 'Ranaut',
+    statusText: 'The daring girl.',
+    image: 'users/Kangana.jpg'
+  },
+  {
+    name: 'Deepika',
+    lastName: 'Padukone',
+    statusText: 'Live,Love & Laugh...',
+    image: 'users/Deepika.jpg'
+  },
+  {
+    name: 'Shraddha',
+    lastName: 'Kapoor',
+    statusText: 'Living the dream!🦄♾🦋💫💜',
+    image: 'users/Shraddha.png'
+  },
+  {
+    name: 'Sara',
+    lastName: 'Ali Khan',
+    statusText: 'Namaste Ji.🙏',
+    image: 'users/Sara.jpg'
+  },
+  {
+    name: 'Rhea',
+    lastName: 'Chakraborty',
+    statusText: 'Smile a lot,because one day all your teeth will fall off😬.#rheality',
+    image: 'users/Rhea.jpg'
+  },
+]
+
